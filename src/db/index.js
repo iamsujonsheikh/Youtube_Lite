fe5ed9ebf0cb29d0248dc7d72daf5c43420a36a5
sujonphero <sujonsheikh.in@gmail.com>
2024-02-09 22:29:54 +0600

    issue on register

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+// import { DB_NAME } from "../constants.js";
 import colors from "colors";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
 
+console.log(process.env.MONGODB_URI)
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log("Database connsction success".bgGreen);
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`Database connection success`.bgGreen);
     } catch (error) {
-        console.log(`Database connection failed. ${error.message}`.bgRed);
+        console.log(`Database connection failed. ${error}`.bgRed);
         process.exit(1);
     }
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
-dotenv.config();
+dotenv.config({
+    path: "./.env"
+});
+
 
 const port = process.env.PORT || 8000;
 
